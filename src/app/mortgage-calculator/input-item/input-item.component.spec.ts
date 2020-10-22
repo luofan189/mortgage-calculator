424762df4b8ed5aa00c5b8d0b7dd0f968556d824
Fan Luo <fan.luo@realjaja.com>
2020-10-22 13:22:19 -0400

    fix test case

--- src/app/mortgage-calculator/input-item/input-item.component.spec.ts
+++ src/app/mortgage-calculator/input-item/input-item.component.spec.ts
@@ -28,6 +28,7 @@ describe('InputItemComponent', () => {
       defaultValue: '1',
       min: 0,
       max: 10,
+      step: 1,
       required: true,
       type: 'type',
       controlType: 'text',
